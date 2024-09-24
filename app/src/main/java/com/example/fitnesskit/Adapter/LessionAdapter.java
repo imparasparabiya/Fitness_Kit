ec88281e50cb42ad2e020ccb273616d492a1ac7f
Prince <parasparabiya@gmail.com>
2024-09-24 17:18:13 +0530

    final Done

--- app/src/main/java/com/example/fitnesskit/Adapter/LessionAdapter.java
+++ app/src/main/java/com/example/fitnesskit/Adapter/LessionAdapter.java
@@ -7,10 +7,8 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bumptech.glide.Glide;
 import com.example.fitnesskit.Modal.Lession;
 import com.example.fitnesskit.databinding.ViewExerciseBinding;
